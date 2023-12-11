837fba59d62a32b71efa23a118f34490012ba209
ViraMesha <wira.mescha@gmail.com>
2023-12-11 02:25:44 +0200

    refactor: Update gap in FlightItemsGrid

--- src/components/organisms/FlightItemsGrid/FlightItemsGrid.styled.tsx
+++ src/components/organisms/FlightItemsGrid/FlightItemsGrid.styled.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  column-gap: 24px;
+  gap: 24px;
 
   & svg {
     fill: none;
